9ae80e102cfb160ac20a9041f298d97469e2fd0d
Filip Pękala <272985@student.pwr.edu.pl>
2024-10-26 16:45:55 +0200

    Dodano skrypt tworzący scheme

---
+++ sql/make_scheme.sql
@@ -0,0 +1,28 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "gene_categories" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"name"	TEXT NOT NULL UNIQUE,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "genes" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"name"	TEXT NOT NULL UNIQUE,
+	"category_id"	INTEGER NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT),
+	CONSTRAINT "genes_gene_categories_fk" FOREIGN KEY("category_id") REFERENCES "gene_categories"("id")
+);
+CREATE TABLE IF NOT EXISTS "ponies" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"name"	TEXT NOT NULL UNIQUE,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "ponies_genes" (
+	"pony_id"	INTEGER NOT NULL,
+	"gene_id"	INTEGER NOT NULL,
+	"gene_category_id"	INTEGER NOT NULL,
+	UNIQUE("gene_category_id","pony_id"),
+	FOREIGN KEY("gene_category_id") REFERENCES "gene_categories"("id"),
+	FOREIGN KEY("gene_id") REFERENCES "genes"("id"),
+	FOREIGN KEY("pony_id") REFERENCES "ponies"("id")
+);
+COMMIT;

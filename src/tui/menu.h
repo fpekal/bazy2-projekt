29115a9ca366773e755175460fae54551811d8b2
Filip Pękala <272985@student.pwr.edu.pl>
2025-01-08 02:24:00 +0100

    Więcej menu

--- src/tui/menu.h
+++ src/tui/menu.h
@@ -37,8 +37,13 @@ public:
 			std::cout << "Wybierz dwa kucyki do walki (oddzielone enterem)" << std::endl;
 			break;
 		case MenuState::SEARCH:
-			std::cout << "Wyszukiwanie" << std::endl;
-
+			std::cout << "Wyszukiwanie\n";
+			std::cout << "1. Wypisz wszystkie kucyki\n";
+			std::cout << "2. Wyszukaj kucyka po imieniu\n";
+			std::cout << "3. Wyszkkaj kucyka po id\n";
+			std::cout << "4. Wyszukaj kucyka statystykach (WIP)\n";
+			std::cout << "5. Wyszukaj losowego kucyka\n";
+			std::cout << "6. Powrot do menu glownego\n";
 			break;
 		}
 	}
@@ -71,6 +76,7 @@ public:
 					Pony parent2_p = load_pony(db, parent2);
 
 					Pony child_p = breed(db, parent1_p, parent2_p, child);
+					draw_pony(child_p);
 					update_pony(db, child_p);
 				}
 				catch (std::exception& e) {
@@ -80,16 +86,90 @@ public:
 			}
 			break;
 		case MenuState::FIGHT:
+			{
+				std::string pony1, pony2;
+
+				std::getline(std::cin, pony1);
+				std::getline(std::cin, pony2);
+
+				try {
+					auto pony1_p = load_pony(db, pony1);
+					auto pony2_p = load_pony(db, pony2);
+					pony1_p.health = pony1_p.get_effective_stats().max_health;
+					pony2_p.health = pony2_p.get_effective_stats().max_health;
+
+					Fight fight(pony1_p, pony2_p);
+
+					while (fight.left_pony.health > 0 && fight.right_pony.health > 0) {
+						fight.make_turn();
+						std::cout << "----------------------------------" << std::endl;
+						draw_pony(fight.left_pony);
+						draw_pony(fight.right_pony);
+						std::cout << "----------------------------------" << std::endl;
+						std::cout << "c - dokończ walkę\n";
+						std::cout << "ENTER - kolejna tura\n";
+						{
+							std::string input;
+							std::getline(std::cin, input);
+
+							if (input == "c") {
+								while (fight.left_pony.health > 0 && fight.right_pony.health > 0) {
+									fight.make_turn();
+								}
+							}
+						}
+					}
+
+					std::cout << "Zwycięża " << (fight.left_pony.health > 0 ? pony1_p.name : pony2_p.name) << std::endl;
+				}
+				catch (std::exception& e) {
+					std::cout << e.what() << std::endl;
+				}
+			}
 			state = MenuState::MAIN;
 			break;
 		case MenuState::SEARCH:
 			{
-				auto ponies = load_all_ponies(db);
-				for (const auto& pony : ponies) {
-					std::cout << pony.name << std::endl;
+				std::string input;
+				std::getline(std::cin, input);
+
+				if (input == "1") {
+					auto ponies = load_all_ponies(db);
+					for (const auto& pony : ponies) {
+						draw_pony(pony);
+						std::cin.get();
+					}
+					break;
+				}
+				if (input == "2") {
+					std::string name;
+					std::getline(std::cin, name);
+					auto pony = load_pony(db, name);
+					draw_pony(pony);
+					break;
+				}
+				if (input == "3") {
+					std::string id;
+					std::getline(std::cin, id);
+					auto pony = load_pony(db, std::stoi(id));
+					draw_pony(pony);
+					break;
+				}
+				if (input == "4") {
+					break;
+				}
+				if (input == "5") {
+					auto ponies = load_all_ponies(db);
+					int random_index = rand() % ponies.size();
+					auto pony = ponies[random_index];
+					draw_pony(pony);
+					break;
+				}
+				if (input == "6") {
+					state = MenuState::MAIN;
+					break;
 				}
 			}
-			state = MenuState::MAIN;
 			break;
 		}
 	}
